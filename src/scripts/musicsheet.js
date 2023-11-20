7b832e103811caacc4fc0ba1a42f59c5cfab7404
SantiagoChamie <s.chamie@uniandes.edu.co>
2023-11-20 17:59:46 -0500

    arreglos tunify 2

--- src/scripts/musicsheet.js
+++ src/scripts/musicsheet.js
@@ -2,17 +2,17 @@
 // Create info
 const notes = new Map();
 const tabs = new Map();
-const notesList = ["A4", "B3", "E2", "E4", "G3", "D3"];
+const notesList = ["A2", "B3", "E2", "E4", "G3", "D3"];
 
 // Add key-value pairs to the hash map
-notes.set("A4", "music/A4.png");
+notes.set("A2", "music/A2.png");
 notes.set("B3", "music/B3.png");
 notes.set("E2", "music/E2.png");
 notes.set("E4", "music/E4.png");
 notes.set("G3", "music/G3.png");
 notes.set("D3", "music/D3.png");
 
-tabs.set("A4", "music/A4TAB.png");
+tabs.set("A2", "music/A2TAB.png");
 tabs.set("B3", "music/B3TAB.png");
 tabs.set("E2", "music/E2TAB.png");
 tabs.set("E4", "music/E4TAB.png");
