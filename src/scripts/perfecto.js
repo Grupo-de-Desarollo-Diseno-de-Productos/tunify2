43cbbeea80122aece1babb184cb180e9a26cac11
Luisfetoga2 <l.torresg@uniandes.edu.co>
2023-11-21 19:38:26 -0500

    ok ahora si sirve, sin bugs (y con colores :D)

--- src/scripts/perfecto.js
+++ src/scripts/perfecto.js
@@ -1,21 +1,40 @@
 var img = document.querySelector('img[src="music/PERFECTO.png"]');
-
+img.style.filter = "hue-rotate(147deg)";
 img.style.opacity = "0";
 
-async function perfecto() {
+var isRunning = false;
+var timer_color, timer_color2;
+var o = 0;
+
+function perfecto() {
+
+    if (isRunning) {
+        clearInterval(timer_color);
+        clearInterval(timer_color2);
+    }
+    isRunning = true;
+
     var img = document.querySelector('img[src="music/PERFECTO.png"]');
-    var o = 0;
-    var timer = setInterval(function() {
+    var hue = Math.floor(Math.random() * 361); 
+    img.style.filter = "hue-rotate("+hue+"deg)";
+    o = 0.01;
+
+    timer_color = setInterval(function() {
         if (o >= 1){
-            clearInterval(timer);
+            clearInterval(timer_color);
+            secondInterval();
         } else {
             img.style.opacity = o;
             o += 0.05;
         }
     }, 10);
-    var timer2 = setInterval(function() {
+}
+
+function secondInterval() {
+    timer_color2 = setInterval(function() {
         if (o <= 0){
-            clearInterval(timer2);
+            clearInterval(timer_color2);
+            isRunning = false;
         } else {
             img.style.opacity = o;
             o -= 0.01;
